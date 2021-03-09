d6febf48309726aaf7ce2f646d2e3027fb4bbf5e
mrebolledo <maxi.rebolledo@gmail.com>
2021-03-09 16:05:17 -0300

    a

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -40,7 +40,7 @@ return [
                 'encrypted' => true,
                 'host' => '127.0.0.1',
                 'port' => 41002,
-                'scheme' => 'https'
+                'scheme' => 'http'
             ],
         ],
 
