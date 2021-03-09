298e7d45e3862ed253b4d675459393d298014e19
mrebolledo <maxi.rebolledo@gmail.com>
2021-03-09 17:39:38 -0300

    a

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,13 +38,9 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'encrypted' => true,
-                'host' => 'dpl.cmatik.app',
+                'host' => '127.0.0.1',
                 'port' => 6001,
-                'scheme' => 'https',
-                'curl_options' => [
-                    CURLOPT_SSL_VERIFYHOST => 0,
-                    CURLOPT_SSL_VERIFYPEER => 0,
-                ]
+                'scheme' => 'https'
             ],
         ],
 
