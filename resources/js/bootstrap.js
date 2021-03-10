63faffc532ee7bddbc0233589256dc7f64ed5616
mrebolledo <maxi.rebolledo@gmail.com>
2021-03-10 11:25:44 -0300

    a

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -31,11 +31,13 @@ import Echo from 'laravel-echo';
 window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
+    auth:{ headers: { 'Authorization': 'Bearer ' + user.token } },
     broadcaster: 'pusher',
-    key: 'dbscerdos',
+    key: process.env.MIX_PUSHER_APP_KEY,
+    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
     wsHost: window.location.hostname,
     wsPort: 6001,
     wssPort: 6001,
-    disableStats: true,
+    disableStats: false,
     enabledTransports: ['ws', 'wss']
 });
