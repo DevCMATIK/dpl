8d8dba5d33f42fd555d2677aeba031aca89ee987
mrebolledo <maxi.rebolledo@gmail.com>
2021-03-10 10:52:45 -0300

    a

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -32,11 +32,9 @@ window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: process.env.MIX_PUSHER_APP_KEY,
-    cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    wsHost: process.env.MIX_PUSHER_HOST,
+    key: 'dbscerdos',
+    wsHost: window.location.hostname,
     wsPort: 6001,
-    forceTLS: false,
+    wssPort: 6001,
     disableStats: true,
-    scheme: process.env.MIX_PUSHER_SCHEME
 });
